6fc80f7f658bbf3bdc59c0344c2a5b8cbf9a7653
NII-o5i2 <51987581+NII-o9i2@users.noreply.github.com>
2019-10-23 19:11:14 +0800

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,3 +1,4 @@
+from module import CarModel
 
 def function1():
     print('this is function1')
@@ -5,3 +6,33 @@ def function1():
 
 def function2():
     print('this is function2')
+
+class ElecCarModel(CarModel):
+    def __init__(self, brand, procedure_data, bq):
+        super().__init__(brand, procedure_data)
+        self.battery_quantity = bq
+
+    def battery_charge(self, bq):
+        self.battery_quantity += bq
+
+    def battery_print(self):
+        print('battery:'+str(self.battery_quantity)+'%')
+
+
+class BatteryClass():
+    def __init__(self, quantity):
+        self.quantity = 0
+
+    def printQuantity(self):
+        print('battery:'+str(self.quantity)+'%')
+
+
+class ElecCarModel2(CarModel):
+    def __init__(self, brand, procedure_data):
+        super().__init__(brand, procedure_data)
+        self.battert_Quantity = BatteryClass(60)
+
+    def print_all(self):
+        super().print_all()
+        print('battery:'+str(self.battert_Quantity.quantity)+'%')
+
