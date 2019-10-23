8d66046fc729d45adc8e16923e88cde84c16a1b7
NII-o5i2 <51987581+NII-o9i2@users.noreply.github.com>
2019-10-23 15:02:05 +0800

    Create module.py

---
+++ module.py
@@ -0,0 +1,14 @@
+def studing():
+    print("studing ...")
+
+
+def overprint(no):
+    for i in range(1, no+1):
+        print('bye bye')
+
+
+def function3():
+    print('this is function3')
+
+def function4():
+    print('this is function4')
