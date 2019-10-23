1e4a347373773fea6a80bc7f1102e378ab3b8886
NII-o5i2 <51987581+NII-o9i2@users.noreply.github.com>
2019-10-23 19:10:56 +0800

    Update module.py

--- module.py
+++ module.py
@@ -12,3 +12,19 @@ def function3():
 
 def function4():
     print('this is function4')
+
+
+
+class CarModel():
+    def __init__(self, brand, procedure_data):
+        self.brand = brand
+        self.procedure_time = procedure_data
+        self.odometers = 0
+
+    def print_all(self):
+        print('brand:'+self.brand.title())
+        print('procedure:'+self.procedure_time.title())
+        print('odometers:'+str(self.odometers))
+
+
+
